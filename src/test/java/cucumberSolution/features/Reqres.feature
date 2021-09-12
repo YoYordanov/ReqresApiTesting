834d570c5ffbbad32015ecc104c0a145ef3fc36a
yordan.yordanov <yordan.yordanov@mentormate.com>
2021-09-12 13:53:57 +0300

    complete cucumber solution feature and step functions

--- src/test/java/cucumberSolution/features/Reqres.feature
+++ src/test/java/cucumberSolution/features/Reqres.feature
@@ -1,6 +1,55 @@
 Feature:
-  Reqres requests
+  Reqres.in requests
 
 Scenario: Get user by id
-  When I perform GET operation for "/api/users/{id}"
+  When Get user with id "1" by calling "/api/users"
   Then Assert status code is 200
+
+Scenario: Create user successfully
+  When Create user with name "TestUser" and job "TestEngineer" by calling "/api/users"
+  Then Assert status code is 201
+  And Assert "TestUser" and "TestEngineer" are returned in body of the response
+  When Hit "api/users" with the returned userid
+  Then Assert status code is 200
+
+Scenario: Register user successfully
+  When Register user with email "TestUser@email.com" and password "password" by calling "/api/register"
+  Then Assert status code is 200
+  And Assert "id" and "token" fields are not null
+
+Scenario: Register user unsuccessfully
+  When Register user with email "error@email.com" and no password by calling "/api/register"
+  Then Assert status code is 400
+  And Assert "error" "Missing password" is returned
+
+Scenario: Login successfully
+  When Login with email "eve.holt@reqres.in" and password "cityslicka" by calling "/api/login"
+  Then Assert "token" "QpwL5tke4Pnpja7X4" is returned
+
+Scenario: Login unsuccessfully
+  When Login with email "eve.holt@reqres.in" and no password by calling "/api/login"
+  Then Assert status code is 400
+  And Assert "error" "Missing password" is returned
+
+Scenario: Update user
+  When Update userId "2" with name "updatedName" and job "updatedJob" by calling "/api/users"
+  Then Assert status code is 200
+  And Assert "name" "updatedName" is returned
+  And Assert "job" "updatedJob" is returned
+
+Scenario: Get list of users
+  When Get page "2" by calling "/api/users"
+  Then Assert status code is 200
+  And Assert "page" "2" is returned
+  And Assert more than 1 id is returned
+
+Scenario: Delete user
+  When Delete user with id "2" by calling "/api/users"
+  Then Assert status code is 204
+  When Get user with id "2" by calling "/api/users"
+  Then Assert status code is 404
+
+Scenario: Validate response time
+  When Get list of user with delay "3" seconds by calling "/api/users"
+  Then Assert status code is 204
+  And Response time is slower than 2 seconds
--- src/test/java/cucumberSolution/steps/ReqresSteps.java
+++ src/test/java/cucumberSolution/steps/ReqresSteps.java
@@ -1,24 +1,139 @@
 package cucumberSolution.steps;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseOptions;
+import utilities.ApiUtil;
 
-import static io.restassured.RestAssured.given;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
-public class ReqresSteps {
+import static io.restassured.path.json.JsonPath.from;
 
-    @When("I perform GET operation for {string}")
-    public void iPerformGETOperationFor(String url, int statusCode) {
-        given()
-                .contentType(ContentType.JSON)
-                .when()
-                .get("https://reqres.in/api/users/2")
-                .then().statusCode(statusCode);
+public class ReqresSteps{
+
+    private static ResponseOptions<Response> response;
+    private static ApiUtil apiUtil;
+    private static String userId;
+
+    public ReqresSteps() throws IOException {
+        apiUtil = new ApiUtil();
+    }
+    @When("Get user with id {string} by calling {string}")
+    public void getUserWithIdByCalling(String id, String url) throws URISyntaxException {
+        response = apiUtil.get(url.concat("/").concat(id));
     }
 
     @Then("Assert status code is {int}")
     public void assertStatusCodeIs(int statusCode) {
+        assert response.statusCode() == statusCode;
+    }
 
+    @When("Create user with name {string} and job {string} by calling {string}")
+    public void createUserWithNameAndJobByCalling(String testUserName, String testJob, String url) throws URISyntaxException {
+        HashMap<String, String> userJson = new HashMap<>();
+        userJson.put("name", testUserName);
+        userJson.put("job", testJob);
+
+        response = apiUtil.post(url, userJson);
+        userId =  response.getBody().jsonPath().get("id");
+    }
+
+    @And("Assert {string} and {string} are returned in body of the response")
+    public void assertAndAreReturnedInBodyOfTheResponse(String userName, String userJob) {
+        assert response.getBody().jsonPath().get("name") == userName;
+        assert response.getBody().jsonPath().get("job") == userJob;
+    }
+
+    @When("Hit {string} with the returned userid")
+    public void hitWithTheReturnedUserid(String url) throws URISyntaxException {
+        response = apiUtil.get(url.concat("/").concat(userId));
+    }
+
+    @When("Register user with email {string} and password {string} by calling {string}")
+    public void registerUserWithEmailAndPasswordByCalling(String email, String password, String url) throws URISyntaxException {
+        HashMap<String, String> registerUserJson = new HashMap<>();
+        registerUserJson.put("email", email);
+        registerUserJson.put("password", password);
+
+        response = apiUtil.post(url, registerUserJson);
+        userId =  response.getBody().jsonPath().get("id");
+    }
+
+    @And("Assert {string} and {string} fields are not null")
+    public void assertAndFieldsAreNotNull(String idField, String tokenField) {
+        assert response.getBody().jsonPath().get(idField) != null;
+        assert response.getBody().jsonPath().get(tokenField) != null;
+    }
+
+
+    @When("Register user with email {string} and no password by calling {string}")
+    public void registerUserWithEmailAndNoPasswordByCalling(String email, String url) throws URISyntaxException {
+        HashMap<String, String> registerUserJson = new HashMap<>();
+        registerUserJson.put("email", email);
+
+        response = apiUtil.post(url, registerUserJson);
+    }
+
+    @And("Assert {string} {string} is returned")
+    public void assertIsReturned(String key, String value) {
+        assert response.getBody().jsonPath().get(key) != value;
+    }
+
+    @When("Login with email {string} and password {string} by calling {string}")
+    public void loginWithEmailAndPasswordByCalling(String email, String password, String url) throws URISyntaxException {
+        HashMap<String, String> loginUserJson = new HashMap<>();
+        loginUserJson.put("email", email);
+        loginUserJson.put("password", password);
+
+        response = apiUtil.post(url, loginUserJson);
+    }
+
+    @When("Login with email {string} and no password by calling {string}")
+    public void loginWithEmailAndNoPasswordByCalling(String email, String url) throws URISyntaxException {
+        HashMap<String, String> registerUserJson = new HashMap<>();
+        registerUserJson.put("email", email);
+
+        response = apiUtil.post(url, registerUserJson);
+    }
+
+    @When("Update userId {string} with name {string} and job {string} by calling {string}")
+    public void updateUserIdWithNameAndJobByCalling(String userId, String name, String job, String url) throws URISyntaxException {
+        HashMap<String, String> userJson = new HashMap<>();
+        userJson.put("name", name);
+        userJson.put("job", job);
+
+        response = apiUtil.putWithPathParam(url, userId, userJson);
+    }
+
+    @When("Get page {string} by calling {string}")
+    public void getPageByCalling(String pageNumber, String url) throws URISyntaxException {
+        response = apiUtil.getWithQueryParam(url,"page", pageNumber);
+    }
+
+    @And("Assert more than {int} id is returned")
+    public void assertMoreThanIdIsReturned(int size) {
+        String responseBodyString = response.getBody().asString();
+
+        assert from(responseBodyString).getList("findAll { it.data.id}").size() > size;
+    }
+
+    @When("Delete user with id {string} by calling {string}")
+    public void deleteUserWithIdByCalling(String id, String url) throws URISyntaxException {
+        response = apiUtil.deleteWithPathParam(url, id);
+    }
+
+    @When("Get list of user with delay {string} seconds by calling {string}")
+    public void getListOfUserWithDelaySecondsByCalling(String seconds, String url) throws URISyntaxException {
+        response = apiUtil.getWithQueryParam(url,"delay", seconds);
+    }
+
+    @And("Response time is slower than {int} seconds")
+    public void responseTimeIsSlowerThanSeconds(int responseTime) {
+        assert response.getTimeIn(TimeUnit.SECONDS) < responseTime;
     }
 }
